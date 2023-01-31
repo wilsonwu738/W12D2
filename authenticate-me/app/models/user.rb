5ef13b690a83abf06a25558630a7b4408fd1e1d4
Logan Hartman <loganchart@gmail.com>
2023-01-31 15:34:34 -0500

    completed user model

--- authenticate-me/app/models/user.rb
+++ authenticate-me/app/models/user.rb
@@ -9,6 +9,26 @@ class User < ApplicationRecord
 
   before_validation :ensure_session_token
   
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    session_token
+  end
+
+  def self.find_by_credentials(credential, password)
+    if URI::MailTo::EMAIL_REGEXP.match?(credential)
+      user = User.find_by(email: credential)
+    else
+      user = User.find_by(username: credential)
+    end
+
+    if user&.authenticate(password)
+      return user
+    else
+      nil
+    end
+  end
+
   private
 
   def generate_unique_session_token
@@ -21,10 +41,6 @@ class User < ApplicationRecord
   def ensure_session_token
     self.session_token ||= generate_unique_session_token
   end
-
-
-
-
 
 
 end
